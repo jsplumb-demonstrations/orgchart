efae95d4a0907e91264b604651a43190b619a475
Simon Porritt <simon@jsplumbtoolkit.com>
2025-04-21 12:05:32 +1000

    - refactored to extend new InspectorComponent available in 7.2.0, much simpler setup.

--- angular/src/app/inspector.component.ts
+++ angular/src/app/inspector.component.ts
@@ -1,7 +1,7 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output} from "@angular/core"
+import {Component, EventEmitter, Output} from "@angular/core"
 
-import { Node, Base, Inspector, Vertex, Edge, JsPlumbToolkit, Surface } from "@jsplumbtoolkit/browser-ui"
-import {DEFAULT_ANGULAR_SURFACE_ID, jsPlumbService} from "@jsplumbtoolkit/browser-ui-angular"
+import { Node, Base, Vertex, Edge } from "@jsplumbtoolkit/browser-ui"
+import {InspectorComponent} from "@jsplumbtoolkit/browser-ui-angular"
 
 @Component({
   template:`
@@ -9,26 +9,27 @@ import {DEFAULT_ANGULAR_SURFACE_ID, jsPlumbService} from "@jsplumbtoolkit/browse
     <div class="jtk-orgchart-inspector" *ngIf="current != null">
       <h1>{{current.data['name']}}</h1>
       <h2>{{current.data['title']}}</h2>
-      <h5 *ngIf="reports.length > 0">Reports:</h5>
-      <app-inspector-person *ngFor="let r of reports" [person]="r" (personSelected)="selectPerson($event)"></app-inspector-person>
-      <h5 *ngIf="manager != null">Reports to:</h5>
-      <app-inspector-person *ngIf="manager != null" [person]="manager" (personSelected)="selectPerson($event)"></app-inspector-person>
-    </div>
-    
-  `,
+      @if(reports.length > 0) {
+        <h5>Reports:</h5>
+        
+        @for(r of reports;track r) {
+          <app-inspector-person [person]="r" (personSelected)="selectPerson($event)"></app-inspector-person>
+        }
+      }
+      @if(manager != null) {
+        <h5>Reports to:</h5>
+        <app-inspector-person [person]="manager" (personSelected)="selectPerson($event)"></app-inspector-person>
+      }
+    </div>`,
   selector:"app-inspector"
 })
-export class InspectorComponent implements AfterViewInit {
+export class OrgChartInspectorComponent extends InspectorComponent {
 
   current!:Node|null
   reports:Array<Vertex> = []
   manager!:Node|null
 
-  @Input() surfaceId:string = DEFAULT_ANGULAR_SURFACE_ID
-
   @Output() personSelected:EventEmitter<Vertex> = new EventEmitter<Vertex>()
-
-  inspector!:Inspector
 
   retrieveDirectReports(person:Node){
     this.reports = person.getSourceEdges().map(e => e.target)
@@ -42,28 +43,17 @@ export class InspectorComponent implements AfterViewInit {
     this.personSelected.emit(person)
   }
 
-  constructor(private $jsplumb:jsPlumbService, private el:ElementRef, private changeDetector:ChangeDetectorRef) { }
+  override reset() {
+    this.current = null
+    this.manager = null
+    this.reports.length = 0
+    super.reset()
+  }
 
-  ngAfterViewInit(): void {
-    this.$jsplumb.getSurface(this.surfaceId, (surface) => {
-      this.inspector = new Inspector({
-        container:this.el.nativeElement,
-        surface,
-        renderEmptyContainer:() => {
-          this.current = null
-          this.manager = null
-          this.reports.length = 0
-          this.changeDetector.detectChanges()
-        },
-        refresh:(obj:Base, cb:() => void) => {
-          this.current = obj as Node
-          this.retrieveDirectReports(this.current)
-          this.getManager(this.current)
-          setTimeout(cb, 0)
-          this.changeDetector.detectChanges()
-        }
-      })
-    })
+  override refresh(obj:Base) {
+    this.current = obj as Node
+    this.retrieveDirectReports(this.current)
+    this.getManager(this.current)
   }
 
 }
